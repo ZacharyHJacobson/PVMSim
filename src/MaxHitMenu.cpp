a1a0b04cd7768074c558b542185eba442fdcdc02
Zachary Jacobson <zach.h.jacobson@gmail.com>
2026-07-11 23:41:48 -0500

    Implement EquipmentSet in MaxHit demo
    MaxHitMenu now uses an EquipmentSet to calculate strength, with three examples.

--- src/MaxHitMenu.cpp
+++ src/MaxHitMenu.cpp
@@ -1,4 +1,5 @@
 #include "maxhitmenu.h"
+#include "equipmentset.h"
 #include <iostream>
 #include "..\\lib\\damagecalc.h"
 
@@ -12,12 +13,21 @@ enum Choices
 void MaxHitMenu::Navigate()
 {
     enum Choices choice = TOP_MENU;
+    EquipmentSet equips;
     while(choice != EXIT)
     {
         switch(choice)
         {
             case DEMO:
-                std::cout << "Max hit: " << damage_calc::physicalMaxHit(118, damage_calc::piety_damage, damage_calc::aggressive_strength_bonus, damage_calc::melee_normal_void_damage, 100) << std::endl;
+                equips.addEquipment(Equipment("Rune scimitar"));
+                std::cout << "Total melee strength: " << equips.getTotalStat(Equipment::MELEE_STRENGTH) << std::endl;
+                std::cout << "Max hit: " << damage_calc::physicalMaxHit(118, damage_calc::piety_damage, damage_calc::aggressive_strength_bonus, damage_calc::melee_normal_void_damage, equips.getTotalStat(Equipment::MELEE_STRENGTH)) << std::endl;
+                equips.addEquipment(Equipment("Barrows gloves"));
+                std::cout << "Total melee strength: " << equips.getTotalStat(Equipment::MELEE_STRENGTH) << std::endl;
+                std::cout << "Max hit: " << damage_calc::physicalMaxHit(118, damage_calc::piety_damage, damage_calc::aggressive_strength_bonus, damage_calc::melee_normal_void_damage, equips.getTotalStat(Equipment::MELEE_STRENGTH)) << std::endl;
+                equips.addEquipment(Equipment("Bronze scimitar"));
+                std::cout << "Total melee strength: " << equips.getTotalStat(Equipment::MELEE_STRENGTH) << std::endl;
+                std::cout << "Max hit: " << damage_calc::physicalMaxHit(118, damage_calc::piety_damage, damage_calc::aggressive_strength_bonus, damage_calc::melee_normal_void_damage, equips.getTotalStat(Equipment::MELEE_STRENGTH)) << std::endl;
                 TopMenu();
                 break;
             case TOP_MENU:

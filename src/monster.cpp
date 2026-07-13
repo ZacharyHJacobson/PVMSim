3ab06cef98e6336cc9437921fb8b97838f487395
Zachary Jacobson <zach.h.jacobson@gmail.com>
2026-07-13 17:49:18 -0500

    Implement missing monster stats
    Monsters now have combat stats, aggressive stats, defensive stats, immunities, and resistances to venom and poison.  New and existing monster stat enums now use prefixed constant names.

--- src/monster.cpp
+++ src/monster.cpp
@@ -2,11 +2,11 @@
 
 Monster::Monster(Bane b, float br, ElementalWeakness e, int wm)
 {
-    if(b == BANELESS && br != 0) throw std::invalid_argument("Bane Resistance requires a Bane");
-    if(e == NO_WEAKNESS && wm != 0) throw std::invalid_argument("Weakness Magnitude requires a Weakness");
-    if(e != NO_WEAKNESS && wm == 0) throw std::invalid_argument("Weakness requires a Weakness Magnitude");
+    if(b == bane_NONE && br != 0) throw std::invalid_argument("Bane Resistance requires a Bane");
+    if(e == ew_NONE && wm != 0) throw std::invalid_argument("Weakness Magnitude requires a Weakness");
+    if(e != ew_NONE && wm == 0) throw std::invalid_argument("Weakness requires a Weakness Magnitude");
     bane = b;
-    bane_resistance = br;
+    resistances[r_BANE] = br;
     elemental_weakness = e;
     weakness_magnitude = wm;
 }
--- src/monster.h
+++ src/monster.h
@@ -5,33 +5,77 @@
 class Monster {
     public:
         enum Bane {
-            BANELESS,
-            DRAGON,
-            DEMON,
-            RAT,
-            UNDEAD,
-            VAMPYRE,
-            GOLEM,
-            LEAF,
-            KALPHITE,
-            CORP,
-            ARAXYTE,
-            XERIC,
-            AMASCUT
+            bane_NONE,
+            bane_DRAGON,
+            bane_DEMON,
+            bane_RAT,
+            bane_UNDEAD,
+            bane_VAMPYRE,
+            bane_GOLEM,
+            bane_LEAF,
+            bane_KALPHITE,
+            bane_CORP,
+            bane_ARAXYTE,
+            bane_XERIC,
+            bane_AMASCUT
         };
         Bane bane;
-        float bane_resistance;
         enum ElementalWeakness {
-            NO_WEAKNESS,
-            AIR,
-            WATER,
-            EARTH,
-            FIRE
+            ew_NONE,
+            ew_AIR,
+            ew_WATER,
+            ew_EARTH,
+            ew_FIRE
         };
         ElementalWeakness elemental_weakness;
         int weakness_magnitude;
-        int armour;
+        enum CombatStat {
+            cs_HITPOINTS,
+            cs_ATTACK,
+            cs_STRENGTH,
+            cs_DEFENCE,
+            cs_MAGIC,
+            cs_RANGED,
+            cs_COUNT     //not a stat
+        };
+        int combat_stats[cs_COUNT];
+        enum AggressiveStat {
+            as_ATTACK,
+            as_STRENGTH,
+            as_MAGIC,
+            as_MAGIC_STRENGTH,
+            as_RANGED,
+            as_RANGED_STRENGTH,
+            as_COUNT     //not a stat
+        };
+        int aggressive_stats[as_COUNT];
+        enum DefensiveStat {
+            ds_STAB,
+            ds_SLASH,
+            ds_CRUSH,
+            ds_MAGIC,
+            ds_LIGHT,
+            ds_MEDIUM,
+            ds_HEAVY,
+            ds_COUNT       //not a stat
+        };
+        int defensive_stats[ds_COUNT];
+        enum Immunity {
+            i_VENOM,           //converts to poison if immune
+            i_POISON,
+            i_CANNON,
+            i_THRALL,
+            i_COUNT    //not a stat
+        };
+        bool immunities[i_COUNT];
+        enum Resistance {
+            r_BANE,
+            r_VENOM,
+            r_POISON,
+            r_COUNT    //not a stat
+        };
+        float resistances[r_COUNT];
         bool wilderness = false;
         bool on_task = true;
-        Monster(Bane b = BANELESS, float br = 0.0, ElementalWeakness e = NO_WEAKNESS, int wm = 0);
+        Monster(Bane b = bane_NONE, float br = 0.0, ElementalWeakness e = ew_NONE, int wm = 0);
 };

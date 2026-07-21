51b092ce779835f0246786ecc20dfc626c86fefc
Zachary Jacobson <zach.h.jacobson@gmail.com>
2026-07-20 23:50:22 -0500

    Implement 2h weapons
    Equipping a 2h weapon will unequip the weapon and shield slots.  Equipping a weapon or shield will unequip the 2h slot.

--- src/equipmentset.cpp
+++ src/equipmentset.cpp
@@ -2,6 +2,15 @@
 
 void EquipmentSet::addEquipment(Equipment e)
 {
+    if(e.GetSlot() == Equipment::eslot_TWOHAND)
+    {
+        equipment[Equipment::eslot_WEAPON] = Equipment();
+        equipment[Equipment::eslot_SHIELD] = Equipment();
+    }
+    else if((e.GetSlot() == Equipment::eslot_WEAPON)||(e.GetSlot() == Equipment::eslot_SHIELD))
+    {
+        equipment[Equipment::eslot_TWOHAND] = Equipment();
+    }
     equipment[e.GetSlot()] = e;
 }
 
